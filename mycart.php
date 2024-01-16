2e07bcb10638b06a2da82ffab70f1bd63ff20a0c
sania <saniashetty235@gmail.com>
2024-01-16 20:24:19 +0530

    changes

---
+++ mycart.php
@@ -0,0 +1,186 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Local-E</title>
+
+  
+<!--  font awesome cdn link -->
+<link rel="icon" href="images/logo-final1.png">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet'>
+<link rel="stylesheet" href=" https://fontawesome.com/">
+<link rel="stylesheet" href="https://fonts.google.com/">
+
+<!-- custom css file link-->
+    <link rel="stylesheet" href="clothing_styles.css"> 
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+</head>
+<style>.theiaStickySidebar:after {content: ""; display: table; clear: both;}</style>
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-Q9KZND01P8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-Q9KZND01P8');
+
+  var lat = '';
+  var long = '';
+</script>
+<body>
+    <!-- head section starts -->
+<header>
+    <a id="scroll-up" class="button_2" href="javascript:void()" title="Go to Top" style="display: block;"><i class="fa fa-angle-up"></i></a>
+        <header class="header main_header stricky_up">
+          <div class="topbar">
+            <div class="top-header">
+                          <div class="col-lg-2 col-md-2 col-xs-6">
+                  <div class="logo border">
+                    <a href="home.html" title="Local-"><img src="images/logo-final1.png" alt="Local-E"></a>
+                  </div>
+                </div>
+                
+              <div class="col-lg-2 col-md-2 hidden-xs">
+                <div class="header_category_1 border">
+                           Shop by Category 
+                </div>
+                </div>
+                
+                <div class="col-lg-4 col-md-3  col-lg-pull-4 col-md-pull-5 col-xs-10">
+                  <div class="search-top">
+                    <form method="get" action="home.html/search">
+                      <div id="sw_search" class="search">
+                        <div class="content-search border">
+                          <input class="autosearch-input" type="text" value="" size="50" placeholder="Search by product..." name="search_keyword" id="search_keyword" required="" autocomplete="off" fdprocessedid="rqv00a" style="height: 20%;">	
+                          <span class="input-group-btn">
+                            <button type="submit" class="fa fa-search button-search-pro form-button" fdprocessedid="usip3"></button>
+                          </span>
+                          <div class="serachListingHeader"></div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+
+                <div class="location border">
+                <!-- <a href="javascript:void();" data-toggle="modal" data-target="#location" title="Choose Location"> -->
+                  <img src="images/location-pin.png" alt="error">
+                <div class="location-text"><p> Location     </p>   
+                 </div>                
+            </div>
+                
+
+                <div class="border"><a href="login.html">Login/Sign-up</a></div>
+                
+                <div class="shopping-cart border">
+                <img src="images/carts.png" alt="error">
+                <div class="shopping-cart-text">
+                  Cart
+                </div>
+                </div>
+                
+          </div>
+          </div>
+        
+
+          
+          <!-- 2nd navbar starts -->                                                                
+         <div class="navbar2">
+          <div class="home second">
+          <a href="clothing.html"  >CLOTHING</a>
+          </div>
+          <div class="furni second">
+         <a href="supermarket.html">SUPERMARKET</a>
+         </div>
+         <div class="custom second">
+         <a href="kitchen_needs.html" >HOME,KITCHEN AND GARDEN</a>
+         </div>
+         <div class="about second">
+         <a href="electronics.html" >ELECTRONICS</a>
+         </div>
+         <div class="contact second">
+         <a href="stationery.html" >STATIONERY</a>
+         </div>
+         
+         
+        </div>
+        <!-- 2nd navbar ends -->
+        </div>
+        </header>
+</header>
+<center>
+    <div class = "container2">
+        <div class="row">
+<div class="col-Ig-12">
+                <div class="mycart">
+                <h2>MY CART</h2>
+                </div>
+            <br><br><br>
+<div class="col-Ig-8">
+  
+            <table class="table">
+                <thead class="text-center">
+                <tr>
+                        <th scope="col">Serial.No</th>
+                        <th scope="col">Item Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col"></th>
+                    </tr>
+                    
+                    
+            </thead>
+            <tbody class="text-center">
+            <?php
+            $total = 0;
+                    if(isset($_SESSION['cart']))
+                    {
+                     foreach($_SESSION['cart'] as $key => $value)
+                     {
+                        $sr=$key+1;
+                        $total=$total+$value['price'];
+                        echo"
+                        <tr>
+                        <td>$sr</td>
+                        <td>$value[item_name]</td>
+                        <td>$value[price]</td>
+                        <td><input type='number'class='text-center' value='$value[Quantity]' min='1' max='10'></td>
+                        <td>
+                            <form action='manage_cart.php' method='POST'>
+                                <button name='Remove_item' class='btn btn-outline-danger'>REMOVE</button>
+                                <input type='hidden' name='item_name' value='$value[item_name]'>
+                            </form>
+                        </td></tr>
+                        ";
+                     }
+                    }
+                    ?>  
+            
+            </table>
+</div>
+<div class="col-Ig-4">
+    <div class='border bg-light rounded'>
+    <h3>Total:</h3>
+    <h4> <?php echo $total ?></h4>
+    <div>
+    <br>
+    <br>
+    <form>
+        <button class='btn btn-primary btn-block'>Make Purchase</button>
+    </form>
+    </div>
+
+</div>
+</div>
+ </div>
+</center>
+</body>
+</html>
---
+++ supermarket_product1.php
@@ -0,0 +1,260 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Local-E</title>
+
+  
+<!--  font awesome cdn link -->
+<link rel="icon" href="images/logo-final1.png">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet'>
+<link rel="stylesheet" href=" https://fontawesome.com/">
+<link rel="stylesheet" href="https://fonts.google.com/">
+
+<!-- custom css file link-->
+    <link rel="stylesheet" href="clothing_styles.css"> 
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+</head>
+<style>.theiaStickySidebar:after {content: ""; display: table; clear: both;}</style>
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-Q9KZND01P8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-Q9KZND01P8');
+
+  var lat = '';
+  var long = '';
+</script>
+<body>
+    <!-- head section starts -->
+<header>
+    <a id="scroll-up" class="button_2" href="javascript:void()" title="Go to Top" style="display: block;"><i class="fa fa-angle-up"></i></a>
+        <header class="header main_header stricky_up">
+          <div class="topbar">
+            <div class="top-header">
+                          <div class="col-lg-2 col-md-2 col-xs-6">
+                  <div class="logo border">
+                    <a href="home.html" title="Local-"><img src="images/logo-final1.png" alt="Local-E"></a>
+                  </div>
+                </div>
+                
+              <div class="col-lg-2 col-md-2 hidden-xs">
+                <div class="header_category_1 border">
+                Shop by Category 
+                </div>
+                </div>
+                
+                <div class="col-lg-4 col-md-3  col-lg-pull-4 col-md-pull-5 col-xs-10">
+                  <div class="search-top">
+                    <form method="get" action="home.html/search">
+                      <div id="sw_search" class="search">
+                        <div class="content-search border">
+                          <input class="autosearch-input" type="text" value="" size="50" placeholder="Search by product..." name="search_keyword" id="search_keyword" required="" autocomplete="off" fdprocessedid="rqv00a" style="height: 20%;">	
+                          <span class="input-group-btn">
+                            <button type="submit" class="fa fa-search button-search-pro form-button" fdprocessedid="usip3"></button>
+                          </span>
+                          <div class="serachListingHeader"></div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+
+                <div class="location border">
+                <!-- <a href="javascript:void();" data-toggle="modal" data-target="#location" title="Choose Location"> -->
+                  <img src="images/location-pin.png" alt="error">
+                <div class="location-text"><p> Location     </p>   
+                 </div>                
+            </div>
+                
+
+                <div class="border"><a href="login.html">Login/Sign-up</a></div>
+                
+                <div class="shopping-cart border">
+                <img src="images/carts.png" alt="error">
+                <div class="shopping-cart-text">
+                <?php
+                        $count=0;
+                        if(isset($_SESSION['cart']))
+                        {
+                            $count=count($_SESSION['cart']);
+                        }
+                    ?>
+
+                    <a href="mycart.php">
+                  Cart (<?php echo $count; ?>)</a>
+                </div>
+                </div>
+                
+          </div>
+          </div>
+        
+
+          
+          <!-- 2nd navbar starts -->                                                                
+         <div class="navbar2">
+          <div class="home second">
+          <a href="clothing.html"  >CLOTHING</a>
+          </div>
+          <div class="furni second">
+         <a href="supermarket.html">SUPERMARKET</a>
+         </div>
+         <div class="custom second">
+         <a href="kitchen_needs.html" >HOME,KITCHEN AND GARDEN</a>
+         </div>
+         <div class="about second">
+         <a href="electronics.html" >ELECTRONICS</a>
+         </div>
+         <div class="contact second">
+         <a href="stationery.html" >STATIONERY</a>
+         </div>
+         
+         
+       </div>
+        <!-- 2nd navbar ends -->
+          </div>
+          </header>
+          </header>
+         
+<!-- 
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+           -->
+            
+<div class="clothing-shops">
+    <h4>Order Online</h4>
+    <div class="cloth-shops">
+      <form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+        <center>
+      <img src="https://images.pexels.com/photos/196643/pexels-photo-196643.jpeg?auto=compress&cs=tinysrgb&w=400" alt="error"></center>
+      <h3>Tomato</h3> 
+      <p class="price">	&#8377; 40</p>
+        <p>1KG tomato</p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Tomato">
+        <input type="hidden" name="price" value="40">
+        
+    </div>
+</form>
+<form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+        <center>
+      <img src="https://images.pexels.com/photos/1306559/pexels-photo-1306559.jpeg?auto=compress&cs=tinysrgb&w=400" alt="error"></center>
+      <h3>Carrot</h3> 
+      <p class="price">&#8377; 35</p>
+        <p>1KG carrot</p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Carrot">
+        <input type="hidden" name="price" value="35">
+        
+    </div>
+</form>
+<form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+      <center><img src="https://images.pexels.com/photos/594137/pexels-photo-594137.jpeg?auto=compress&cs=tinysrgb&w=400" alt="error">
+      </center>
+      <h3>Capsicum</h3> 
+      <p class="price">&#8377; 50</p>
+        <p>1KG Capsicum-yellow</p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Capsicum">
+        <input type="hidden" name="price" value="50">
+    </div>
+</form>
+<form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+     <center>
+      <img src="https://images.pexels.com/photos/4207652/pexels-photo-4207652.jpeg?auto=compress&cs=tinysrgb&w=400" alt="error"></center>
+      <h3>Eggs</h3> 
+      <p class="price">&#8377; 70</p>
+        <p>1 dozen eggs</p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Eggs">
+        <input type="hidden" name="price" value="70">
+    </div>
+</form>
+<form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+     <center>
+      <img src="https://images.pexels.com/photos/3688/food-dinner-lunch-chicken.jpg?auto=compress&cs=tinysrgb&w=400" alt="error"></center>
+      <h3>Chicken meat</h3> 
+      <p class="price">&#8377;150 </p>
+        <p>1KG chicken meat</p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Chicken meat">
+        <input type="hidden" name="price" value="150">
+    </div>
+</form>
+<form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+    <center>
+      <img src="https://images.pexels.com/photos/618775/pexels-photo-618775.jpeg?auto=compress&cs=tinysrgb&w=400" alt="error">
+      </center>
+      <h3>Mutton meat</h3> 
+      <p class="price">&#8377; 190</p>
+        <p>1KG mutton meat</p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Mutton meat">
+        <input type="hidden" name="price" value="190">
+    </div>
+</form>
+<form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+ <center>
+      <img src="https://images.pexels.com/photos/236010/pexels-photo-236010.jpeg?auto=compress&cs=tinysrgb&w=400" alt="error">
+      </center>
+      <h3>Milk</h3> 
+      <p class="price">&#8377; 60</p>
+        <p>1LT low-fat milk</p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Milk">
+        <input type="hidden" name="price" value="60">
+      
+    </div>
+</form>
+<form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+    <center>
+      <img src="https://images.pexels.com/photos/6805775/pexels-photo-6805775.jpeg?auto=compress&cs=tinysrgb&w=400" alt="error"></center>
+      <h3>Paneer</h3> 
+      <p class="price">&#8377; 80</p>
+        <p>500gm Paneer </p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Paneer">
+        <input type="hidden" name="price" value="80">
+    </div>
+</form>
+<form action ="manage_cart.php" method="POST">
+    <div class="clothing-shop1">
+     <center>
+      <img src="https://images.pexels.com/photos/2262832/pexels-photo-2262832.jpeg?auto=compress&cs=tinysrgb&w=400" alt="error"></center>
+      <h3>Coffee Powder </h3> 
+      <p class="price">&#8377; 400</p>
+        <p>1KG coffee powder</p>
+        <center><p><button type="submit" name="Add_to_Cart">Add to Cart</button></p></center>
+        <input type="hidden" name="item_name" value="Coffee Powder">
+        <input type="hidden" name="price" value="400">
+    </div>
+</form>
+  </div>
+  </div>
+  
+
+          </body>
+          </html>
